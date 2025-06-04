09163ef5949c28dab207db1c4a21294464b65bc0
acc0mplish <70960296+acc0mplish@users.noreply.github.com>
2025-06-04 16:00:31 +0900

    Update date_time_test.go

--- calendar/remote/date_time_test.go
+++ calendar/remote/date_time_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2025-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 package remote
@@ -18,28 +18,28 @@ func TestDateTime_Time(t *testing.T) {
 		expectedPrettyString string
 	}{
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.0000005", ""},
-			expectedUTC:          "2019-11-19T01:00:00.0000005Z",
-			expectedString:       "2019-11-19T01:00:00Z",
-			expectedPrettyString: "19 Nov 19 01:00 UTC",
+			DateTime:             DateTime{"2025-11-19T01:00:00.0000005", ""},
+			expectedUTC:          "2025-11-19T01:00:00.0000005Z",
+			expectedString:       "2025-11-19T01:00:00Z",
+			expectedPrettyString: "19 Nov 25 01:00 UTC",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.9999999", "Asia/Hong_Kong"},
-			expectedUTC:          "2019-11-18T17:00:00.9999999Z",
-			expectedString:       "2019-11-19T01:00:00+08:00",
-			expectedPrettyString: "19 Nov 19 01:00 HKT",
+			DateTime:             DateTime{"2025-11-19T01:00:00.9999999", "Asia/Hong_Kong"},
+			expectedUTC:          "2025-11-18T17:00:00.9999999Z",
+			expectedString:       "2025-11-19T01:00:00+08:00",
+			expectedPrettyString: "19 Nov 25 01:00 HKT",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.9999999", "China Standard Time"},
-			expectedUTC:          "2019-11-18T17:00:00.9999999Z",
-			expectedString:       "2019-11-19T01:00:00+08:00",
-			expectedPrettyString: "19 Nov 19 01:00 CST",
+			DateTime:             DateTime{"2025-11-19T01:00:00.9999999", "China Standard Time"},
+			expectedUTC:          "2025-11-18T17:00:00.9999999Z",
+			expectedString:       "2025-11-19T01:00:00+08:00",
+			expectedPrettyString: "19 Nov 25 01:00 CST",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T14:00:00.0000005", "MST"},
-			expectedUTC:          "2019-11-19T21:00:00.0000005Z",
-			expectedString:       "2019-11-19T14:00:00-07:00",
-			expectedPrettyString: "19 Nov 19 14:00 MST",
+			DateTime:             DateTime{"2025-11-19T14:00:00.0000005", "MST"},
+			expectedUTC:          "2025-11-19T21:00:00.0000005Z",
+			expectedString:       "2025-11-19T14:00:00-07:00",
+			expectedPrettyString: "19 Nov 25 14:00 MST",
 		},
 	} {
 		t.Run(tc.TimeZone, func(t *testing.T) {
@@ -59,39 +59,39 @@ func TestDateTime_In(t *testing.T) {
 		expectedPrettyString string
 	}{
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.0000005", ""},
+			DateTime:             DateTime{"2025-11-19T01:00:00.0000005", ""},
 			toTimezone:           "Pacific Standard Time",
-			expectedUTC:          "2019-11-19T01:00:00.0000005Z",
-			expectedString:       "2019-11-18T17:00:00-08:00",
-			expectedPrettyString: "18 Nov 19 17:00 PST",
+			expectedUTC:          "2025-11-19T01:00:00.0000005Z",
+			expectedString:       "2025-11-18T17:00:00-08:00",
+			expectedPrettyString: "18 Nov 25 17:00 PST",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.9999999", "Asia/Hong_Kong"},
+			DateTime:             DateTime{"2025-11-19T01:00:00.9999999", "Asia/Hong_Kong"},
 			toTimezone:           "Mountain Standard Time",
-			expectedUTC:          "2019-11-18T17:00:00.9999999Z",
-			expectedString:       "2019-11-18T10:00:00-07:00",
-			expectedPrettyString: "18 Nov 19 10:00 MST",
+			expectedUTC:          "2025-11-18T17:00:00.9999999Z",
+			expectedString:       "2025-11-18T10:00:00-07:00",
+			expectedPrettyString: "18 Nov 25 10:00 MST",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T01:00:00.9999999", "China Standard Time"},
+			DateTime:             DateTime{"2025-11-19T01:00:00.9999999", "China Standard Time"},
 			toTimezone:           "Eastern Standard Time",
-			expectedUTC:          "2019-11-18T17:00:00.9999999Z",
-			expectedString:       "2019-11-18T12:00:00-05:00",
-			expectedPrettyString: "18 Nov 19 12:00 EST",
+			expectedUTC:          "2025-11-18T17:00:00.9999999Z",
+			expectedString:       "2025-11-18T12:00:00-05:00",
+			expectedPrettyString: "18 Nov 25 12:00 EST",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T14:00:00.0000005", "MST"},
+			DateTime:             DateTime{"2025-11-19T14:00:00.0000005", "MST"},
 			toTimezone:           "Eastern Standard Time",
-			expectedUTC:          "2019-11-19T21:00:00.0000005Z",
-			expectedString:       "2019-11-19T16:00:00-05:00",
-			expectedPrettyString: "19 Nov 19 22:00 EST",
+			expectedUTC:          "2025-11-19T21:00:00.0000005Z",
+			expectedString:       "2025-11-19T16:00:00-05:00",
+			expectedPrettyString: "19 Nov 25 22:00 EST",
 		},
 		{
-			DateTime:             DateTime{"2019-11-19T14:00:00.0000005", "MST"},
+			DateTime:             DateTime{"2025-11-19T14:00:00.0000005", "MST"},
 			toTimezone:           "Central European Standard Time",
-			expectedUTC:          "2019-11-19T21:00:00.0000005Z",
-			expectedString:       "2019-11-19T22:00:00+01:00",
-			expectedPrettyString: "18 Nov 19 17:00 CEST",
+			expectedUTC:          "2025-11-19T21:00:00.0000005Z",
+			expectedString:       "2025-11-19T22:00:00+01:00",
+			expectedPrettyString: "18 Nov 25 17:00 CEST",
 		},
 	} {
 		t.Run(tc.toTimezone, func(t *testing.T) {
